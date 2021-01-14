fa5345641700e906c154f3e30d27dc59bdad3334
Bald Natty <cwebster@nuvizz.com>
2021-01-13 19:30:32 -0500

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Fiyah Bun!'));
+app.get('/', (req, res) => res.send('When Jah Come.... Fiyah Bun!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
